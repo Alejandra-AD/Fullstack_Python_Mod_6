9158b15aacb6ec338e8127f28c769d5556558112
Alejandra C. Araya Díaz <121683371+Aleiku0@users.noreply.github.com>
2023-06-18 21:26:43 -0300

    Add files via upload

---
+++ ABP_M6_A.3_Alejandra_AD/kallejon_manga/kallejon_manga/urls.py
@@ -0,0 +1,29 @@
+"""
+URL configuration for kallejon_manga project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from kallejon_manga_app.views import lista_usuario,HomeView,MaintenanceView,ProductsView,StoresView,DeliveryView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('usuarios/',lista_usuario,name='usuarios'),
+    path('', HomeView.as_view(), name='Home'),
+    path('mantenimiento/',MaintenanceView.as_view(), name='mantenimiento'),
+    path('productos/',ProductsView.as_view(), name='productos'),
+    path('tiendas/',StoresView.as_view(), name='tiendas'),
+    path('despacho/',DeliveryView.as_view(), name='envios'),
+]
